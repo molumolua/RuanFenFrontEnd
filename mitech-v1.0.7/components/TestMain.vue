d8dccfe770ef612eb5222bbdff3dd2df41dfaeed
molu <1005389104@qq.com>
2023-11-28 20:39:01 +0800

    add test!!!

--- mitech-v1.0.7/components/TestMain.vue
+++ mitech-v1.0.7/components/TestMain.vue
@@ -3,9 +3,13 @@
         hhhh!!!
         <li>error!!</li>
         wuwuwu!!
+        <div class="btn test_todo">
+            test!!
+        </div>
     </div>
 </template>
 <script>
+import axios from "axios"
 export default {
     mounted() {
         var color=useColor()
@@ -13,9 +17,29 @@ export default {
         color.value="black"
         var color2=useColor()
         console.log(color2.value)
-    
+        
+
+
+        const data = ref(null)
+        data.value=fetchData()
+        console.log(data)
     }
 
 }
-
+const fetchData = async () => {
+    try {
+        const response = await axios.get('https://api.example.com/data');
+        // 处理响应
+    } catch (error) {
+        // 处理错误
+    }
+}
 </script>
+<style lang="scss" scoped>
+
+.test_todo{
+    display: table-cell;
+    font-size: 20px;
+}
+
+</style>
